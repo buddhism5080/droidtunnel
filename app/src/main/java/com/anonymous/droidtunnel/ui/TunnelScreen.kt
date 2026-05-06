f5931e5ff050b29a64a2ab181ead0cb24f012bcb
buddhism5080 <million66668888@gmail.com>
2026-05-06 17:42:45 +0800

    fix: remove incompatible lazy item import

--- app/src/main/java/com/anonymous/droidtunnel/ui/TunnelScreen.kt
+++ app/src/main/java/com/anonymous/droidtunnel/ui/TunnelScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
